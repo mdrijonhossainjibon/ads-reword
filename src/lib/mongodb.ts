3dc42791dc603b72a44636e53ea05f8a175c2657
mdrijonhossainjibon <arntoken.tem@gmail.com>
2025-02-05 15:44:13 +0600

    db up k

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ads-tg';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://admin:admin@atlascluster.nei8u.mongodb.net/ads-tg';
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env');
